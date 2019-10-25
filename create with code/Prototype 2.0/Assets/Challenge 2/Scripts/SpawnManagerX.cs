27592cd7e8537a67e2254a80ba1cd871591b1059
Gordon <gordon.r141@stu.nebo.edu>
2019-10-25 10:27:23 -0600

    objects destroy on contact and out of bounds.
    started first challenge

--- create with code/Prototype 2.0/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ create with code/Prototype 2.0/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -16,17 +16,20 @@ public class SpawnManagerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke("SpawnRandomBall", startDelay, spawnInterval);
+        
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
     }
 
 }
