09632fae9437d70006c6d6366e5a43153919c1dc
Gordon <gordon.r141@stu.nebo.edu>
2019-12-19 10:27:34 -0700

    Added text

--- create with code/Prototype 5/Assets/Course Library/scripts/Target.cs
+++ create with code/Prototype 5/Assets/Course Library/scripts/Target.cs
@@ -10,7 +10,16 @@ public class Target: MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
